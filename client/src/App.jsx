8dbd80f1bfe2ad92edc53e4ae0cdd42873aaa1bc
SiddharthP711 <sid711p@gmail.com>
2025-01-04 15:00:13 -0500

    Setup Mantine and Judging Page
    Installed Mantine component library and created the basic Judging Page component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import NavBar from './Components/navbar';
+import React from "react";
+import "./App.css";
+import NavBar from "./Components/navbar";
+import JudgingPage from "./Components/JudgingPage/JudgingPage";
 
 function App() {
   return (
     <>
       <NavBar />
+      <JudgingPage></JudgingPage>
     </>
   );
 }
---
+++ client/src/Components/JudgingPage/JudgingPage.jsx
@@ -0,0 +1,11 @@
+import { Rating } from "@mantine/core";
+import "./JudgingPage.css";
+
+function JudgingPage() {
+  return (
+    <main>
+      <Rating defaultValue={2} color="green" />
+    </main>
+  );
+}
+export default JudgingPage;
