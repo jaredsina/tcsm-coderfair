1a515fc4d27fde4b93da109f5c7dcd2e6b83c732
Joshy14 <97353253+Joshy14@users.noreply.github.com>
2025-01-14 17:39:32 -0500

    Attempt to fix fix card width

--- client/src/Components/FeaturedProject.jsx
+++ client/src/Components/FeaturedProject.jsx
@@ -1,19 +1,23 @@
 import React from "react"
 import './featuredProject.css'
-import { Card, Group, Text, ActionIcon, Image, SimpleGrid, rem, CardSection } from '@mantine/core';
+import { Card, Image, Text, CardSection} from '@mantine/core';
 const FeaturedProject = ()=>{
     return(
         <>
-        <Card withBorder shadow="md" radius="md">
+        <div classname="wrapper">
+        <Card withBorder  shadow="md" radius="md">
             <CardSection withBorder inheritPadding py="xs">
-                <Text fw={500}>Project</Text>
+                <Text weight={100} fw={500}>Project</Text>
             </CardSection>
-            <Card.Section mt="sm">
-                <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-4.png" />
+            <Card.Section __size="sm" mt="sm">
+                <Image weight={100}height={100} src="" />
             </Card.Section>
         </Card>
+        </div>
         </>
     );
 };
 
+//I tried everything, cannot adjust the width. Maybe it has to do with making some sort of wrapper
+//so far all that has worked with sizing is using height={int} on image component, but nowhere else.
 export default FeaturedProject;
