a00f5e7f29b8b595911ddf7d5197d20af613d3e8
SiddharthP711 <sid711p@gmail.com>
2025-01-25 13:59:49 -0500

    Created the Judging Page with mobile and desktop styles
    I created the Judging page components using Mantine's Flex box. I also made the components change size based on the size of the screen.

--- client/src/Components/JudgingPage/JudgingPage.jsx
+++ client/src/Components/JudgingPage/JudgingPage.jsx
@@ -1,11 +1,120 @@
-import { Rating } from "@mantine/core";
 import "./JudgingPage.css";
+import {
+  Button,
+  Flex,
+  Textarea,
+  Text,
+  List,
+  TextInput,
+  Rating,
+  Title,
+} from "@mantine/core";
+
+export function Judging() {
+  return (
+    <Title className="PageTitle" ta="center" order={1}>
+      Judging
+    </Title>
+  );
+}
+export function Form() {
+  return (
+    <Text size="xl" fw={500} Semibold>
+      Judge Form
+    </Text>
+  );
+}
+
+export function Podium() {
+  return (
+    <Text size="xl" fw={500} Semibold>
+      Top Coder
+    </Text>
+  );
+}
 
 function JudgingPage() {
   return (
     <main>
-      <Rating defaultValue={2} color="green" />
+      <Judging />
+      <Flex
+        direction={"row-reverse"}
+        gap="md"
+        justify="center"
+        align="center"
+        wrap={"wrap-reverse"}
+      >
+        <Flex direction={"column"} gap="md">
+          <Podium />
+          <Flex direction={"row"} align="flex-end">
+            <span className="Rectangle"></span>
+            <span className="Podium"></span>
+            <span className="Bar"></span>
+          </Flex>
+        </Flex>
+        <Flex direction={"column"} gap="md">
+          <Form />
+          <Flex direction={"row"} gap={"md"}>
+            <List type="ordered">
+              <Flex
+                direction={{ base: "column", md: "row" }}
+                gap={{ base: "15", md: "50px" }}
+              >
+                <Flex direction={"column"} gap={"md"}>
+                  <List.Item>
+                    <div>Your Name</div>
+                    <TextInput placeholder="Enter Your Response" />
+                  </List.Item>
+                  <List.Item>
+                    <div>Game Name</div>
+                    <TextInput placeholder="Enter Your Response" />
+                  </List.Item>
+                  <List.Item>
+                    <div>Name of Coder</div>
+                    <TextInput placeholder="Enter Your Response" />
+                  </List.Item>
+                  <List.Item>
+                    <div>Programing Language</div>
+                    <TextInput placeholder="Enter Your Response" />
+                  </List.Item>
+                </Flex>
+                <Flex direction={"column"} gap={"md"} flex>
+                  <List.Item>
+                    <div>Quality</div>
+                    <Rating defaultValue={0} color="green" />
+                  </List.Item>
+                  <List.Item>
+                    <div>Creativity</div>
+                    <Rating defaultValue={0} color="green" />
+                  </List.Item>
+                  <List.Item>
+                    <div>Complexity</div>
+                    <Rating defaultValue={0} color="green" />
+                  </List.Item>
+                  <List.Item>
+                    <div>Presentation</div>
+                    <Rating defaultValue={0} color="green" />
+                  </List.Item>
+                  <List.Item>
+                    <Textarea size="md" radius="lg" label="Overall Review" />
+                  </List.Item>
+                  <Button
+                    variant="filled"
+                    color="green"
+                    size="md"
+                    className="Submit"
+                    w="fit-content"
+                  >
+                    Submit
+                  </Button>
+                </Flex>
+              </Flex>
+            </List>
+          </Flex>
+        </Flex>
+      </Flex>
     </main>
   );
 }
+
 export default JudgingPage;
