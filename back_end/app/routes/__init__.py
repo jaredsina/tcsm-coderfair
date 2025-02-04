aaf4d0f13e2726c171a40ab64817334a53388108
Shriyap19 <shriyapatle36@gmail.com>
2025-02-04 17:27:11 -0500

    Deleting routes(userroles,role,question and questiongrade)

--- back_end/app/routes/__init__.py
+++ back_end/app/routes/__init__.py
@@ -3,12 +3,8 @@
 from .user_routes import user_routes
 from .projects_routes import projects_routes
 from .coderfair_routes import coderfair_routes
-from .role_routes import role_routes
-from .question_routes import question_routes
-from .userroles_routes import userroles_routes
 from .judge_routes import judge_routes
 
-from .questiongrade_routes import questiongrade_routes
 
 
 # Registering blueprints (routes) here
@@ -17,8 +13,4 @@ def init_routes(app):
     app.register_blueprint(user_routes, url_prefix="/users")
     app.register_blueprint(projects_routes, url_prefix="/projects")
     app.register_blueprint(judge_routes, url_prefix="/judges")
-    app.register_blueprint(questiongrade_routes, url_prefix="/questiongrades")
     app.register_blueprint(coderfair_routes, url_prefix="/coderfair")
-    app.register_blueprint(role_routes, url_prefix="/roles")
-    app.register_blueprint(question_routes, url_prefix="/questions")
-    app.register_blueprint(userroles_routes, url_prefix="/userroles")
