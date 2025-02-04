5606c2057390e00ede4ce29d644e075c384d234e
Joshy14 <97353253+Joshy14@users.noreply.github.com>
2025-02-04 23:16:38 +0000

    Make Footer a component

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Container, Text, Anchor } from "@mantine/core"
+export function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#1a1a1a', color: 'white', padding: '20px 0' }}>
+      <Container size="md" style={{ textAlign: 'center' }}>
+        <Anchor href="https://www.thecoderschool.com/locations/montgomery/" >The Coder School Montgomery</Anchor>
+        <Text>Got a question? Email us at <Anchor href="mailto:montgomery@thecoderschool.com" >montgomery@thecoderschool.com</Anchor></Text>
+
+        <Text size="sm">&copy; 2014-2022, the Coder School San Francisco, LLC. All Rights, Alright?
+          Don't forget our<Anchor href="https://www.thecoderschool.com/privacy/"> Privacy Policy</Anchor></Text>
+      </Container>
+    </footer>
+  )
+}
