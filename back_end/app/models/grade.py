59d829081e9a0123465e864028e72138ca3ddc69
posaune14 <michaelyoutz3@gmail.com>
2025-02-04 13:16:10 -0500

    creating grade model

---
+++ back_end/app/models/grade.py
@@ -0,0 +1,32 @@
+from flask_pymongo import PyMongo
+
+class GradeModel:
+  def __init__(self, mongo: PyMongo):
+    self.collection = mongo.cx["test"]["grades"]
+
+  def create_grade(self, id, judge_id, project_id, overall_comments):
+    grade_data = {
+      "id": id,
+      "judge_id": judge_id,
+      "project_id": project_id,
+      "overall_comments": overall_comments,
+    }
+    result = self.collection.insert_one(grade_data)
+    return str(result.inserted_id)
+  
+  def find_grade_by_id(self, id):
+    return self.collection.find_one({"_id": id})
+
+  def list_project_grades(self, project_id):
+    return list(self.collection.find({"project_id": project_id}))
+  
+  def list_judge_grades(self, judge_id):
+    return list(self.collection.find({"judge_id": judge_id}))
+  
+  def update_grade(self, id, update_data):
+    result = self.collection.update_one({"_id": id}, {"$set": update_data})
+    return result
+  
+  def delete_grade(self, id):
+    result = self.collection.delete_one({"_id": id})
+    return result
