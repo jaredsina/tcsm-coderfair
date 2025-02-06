d2547db30a45cf6b68279500a6cccd0148f01846
Joshy14 <97353253+Joshy14@users.noreply.github.com>
2025-02-06 01:38:38 +0000

    import mantine Notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import App from "./App.jsx";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import '@mantine/carousel/styles.css';
+import { Notifications } from '@mantine/notifications';
+import '@mantine/notifications/styles.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
