13485a743f001c22a1acb2fe1fc1ba83a3124e8c
posaune14 <michaelyoutz3@gmail.com>
2025-02-07 15:52:05 -0500

    add is_admin and is_staff properties

--- back_end/app/models/user.py
+++ back_end/app/models/user.py
@@ -5,13 +5,16 @@ class UserModel:
     def __init__(self, mongo: PyMongo):
         self.collection = mongo.cx["test"]["users"]
 
-    def create_user(self, id, first_name, last_name, email, username):
+    def create_user(self, id, first_name, last_name, email, username, is_admin, is_staff):
         user_data = {
             "id": id,
             "first_name": first_name,
             "last_name": last_name,
             "email": email,
             "username": username,
+            #is_admin and is_staff are booleans
+            "is_admin": is_admin,
+            "is_staff": is_staff,
         }
         result = self.collection.insert_one(user_data)
         return str(result.inserted_id)
@@ -21,6 +24,12 @@ class UserModel:
 
     def find_user_by_id(self, id):
         return self.collection.find_one({"id": id})
+
+    def list_admins(self, is_admin):
+        return self.collection.find({"is_admin": is_admin})
+    
+    def list_staff(self, is_staff):
+        return self.collection.find({"is_staff": is_staff})
 
     def list_users(self):
         return list(self.collection.find())
