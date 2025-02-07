c4dcb3ce92e1d944391b5a05c7945525ac6df5a5
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-02-07 12:55:01 -0500

    Update error_handlers.py
    Print the errors so we can view them in the server logs

--- back_end/app/routes/error_handlers.py
+++ back_end/app/routes/error_handlers.py
@@ -6,21 +6,25 @@ def init_error_handlers(app):
     # Global 400 Error Handler
     @app.errorhandler(400)
     def handle_bad_request(e):
+        print(e)
         return jsonify({"message": "Bad Request", "error": str(e)}), 400
 
     # Global 404 Error Handler
     @app.errorhandler(404)
     def handle_not_found(e):
+        print(e)
         return jsonify({"message": "Route Not Found"}), 404
 
     # Global 500 Error Handler
     @app.errorhandler(500)
     def handle_internal_error(e):
+        print(e)
         return jsonify({"message": "Internal Server Error"}), 500
 
     # Global Exception Handler (Catches all unhandled errors)
     @app.errorhandler(Exception)
     def handle_unexpected_error(e):
+        print(e)
         return jsonify({"message": "An unexpected error occurred"}), 500
 
     app.register_error_handler(400, handle_bad_request)
