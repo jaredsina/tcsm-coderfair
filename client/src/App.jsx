1996f56f0643900e42f406f67a0dbd72eebfc856
SiddharthP711 <sid711p@gmail.com>
2025-02-08 14:39:56 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from "react-route
 import ProjectPage from "./Pages/ProjectPage";
 import Results from "./Pages/Results";
 import Account from "./Pages/AccountPage/AccountPage";
-import Home from "./Pages/HomePage/HomePage";  
+import Home from "./Pages/HomePage/HomePage";
+import SignIn from "./Pages/SignIn"; // Import the Sign-In page
 
 function App() {
   // Custom Hook to show/hide NavBar based on the current route
@@ -15,18 +16,18 @@ function App() {
 
   return (
     <>
-      {showNavBar && <NavBar />} {/* Only show NavBar if not on the SignIn page */}
+      {showNavBar && <NavBar />}{' '}
+      {/* Only show NavBar if not on the SignIn page */}
       <Routes>
         {/* Default route is the Sign-In page */}
-        <Route path="/" element={<SignIn />} /> 
+        <Route path="/" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
         <Route path="/results" element={<Results />} />
         <Route path="/projects" element={<ProjectPage />} />
         <Route path="/account" element={<Account />} />
         <Route path="/judging" element={<JudgingPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
-      
     </>
   );
 }
