94dbaf694adb42fd70f4d352913f85b16eb729dd
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-02-12 17:59:27 -0500

    Setup judge redux and notifications
    Create actions for create,get,update,deleting judges.
    
    Install mantine notifications to display server request operations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import '@mantine/notifications/styles.css';
 import App from "./App.jsx";
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
@@ -9,10 +10,12 @@ import { MantineProvider } from "@mantine/core";
 import { Provider } from "react-redux";
 import {store} from "./store/store.js";
 import '@mantine/carousel/styles.css';
+import { Notifications } from "@mantine/notifications";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MantineProvider>
+      <Notifications />
       <Provider store={store}>
         <App />
       </Provider>
