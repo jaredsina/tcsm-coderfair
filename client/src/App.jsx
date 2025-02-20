89bee737d2c27e5aab9eb58e664efae09b6d5a58
Joshy14 <97353253+Joshy14@users.noreply.github.com>
2025-02-20 01:53:37 +0000

    Footer attempt to fix, Unsuccessfull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Home from "./Pages/HomePage/HomePage";
 import SignIn from "./Pages/SignIn"; // Import the Sign-In page
 import Reset from "./Pages/ResetPass/Reset";
 import Footer from "./Components/Footer";
+import { AppShell } from "@mantine/core";
 function App() {
   // Custom Hook to show/hide NavBar based on the current route
   const location = useLocation();
@@ -17,18 +18,22 @@ function App() {
 
   return (
     <>
-      {showElements && <NavBar />} {/* Only show NavBar if not on the SignIn page */}
-      <Routes>
-        {/* Default route is the Sign-In page */}
-        <Route path="/" element={<SignIn />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/projects" element={<ProjectPage />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/judging" element={<JudgingPage />} />
-        <Route path="/reset" element={<Reset />} />
-      </Routes>
-      {showElements && <Footer />}
+      <AppShell style={{ minHeight: "100vh" }} footer={<Footer />}>
+        {showElements && <NavBar />} {/* Only show NavBar if not on the SignIn page */}
+        <div style={{ flexGrow: 1 }}>
+          <Routes>
+            {/* Default route is the Sign-In page */}
+            <Route path="/" element={<SignIn />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/projects" element={<ProjectPage />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/judging" element={<JudgingPage />} />
+            <Route path="/reset" element={<Reset />} />
+          </Routes>
+        </div>
+        {showElements && <Footer />}
+      </AppShell>
     </>
   );
 }
