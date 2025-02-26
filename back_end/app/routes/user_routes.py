09c2c99c82318bd72347c8aac8e323cfa020297d
nikitam5 <nikita.k.mathur@gmail.com>
2025-02-26 17:47:38 -0500

    i deleted /user/ from url prefix

--- back_end/app/routes/user_routes.py
+++ back_end/app/routes/user_routes.py
@@ -2,26 +2,27 @@
 from ..models.user import UserModel
 from flask import Blueprint, jsonify, request, current_app
 from flask_bcrypt import Bcrypt
-
+from bson import ObjectId
 
 user_routes = Blueprint("user_routes", __name__)
 
 
 # # Define a simple route inside this blueprint
-# @user_routes.route('/user/<string:user_id>')
-# def get_users(user_id):
-#     try:
-#         user_id = ObjectId(user_id)
-#         user_model = UserModel(current_app.mongo)
-#         user = user_model.find_user_by_id(user_id)
-#     #if not user:
-#         #return jsonify({"error" : str(e)}), 500
-#     except Exception as e:
-#         user["_id"] = str(user["_id"])
-#         return jsonify(user), 200
+@user_routes.route('/<string:user_id>')
+def get_user(user_id):
+    try:
+        user_id = ObjectId(user_id)
+        user_model = UserModel(current_app.mongo)
+        user = user_model.find_user_by_id(user_id)
+
+    #if not user:
+        #return jsonify({"error" : str(e)}), 500
+    except Exception as e:
+        return jsonify({"message":"error getting user", "error": str(e)}), 400
+    return jsonify(user), 200
 
 
-# route for creating users
+#route for creating users
 @user_routes.route("/create/", methods=["POST"])
 def create_users():
     try:
@@ -38,7 +39,7 @@ def create_users():
 
         new_user = UserModel(current_app.mongo)
         response = new_user.create_user(
-            first_name, last_name, email, username, is_admin, is_staff, hashed_password
+            first_name, last_name, email, is_admin, is_staff, username, hashed_password
         )
 
     except Exception as e:
@@ -48,7 +49,7 @@ def create_users():
     ), 201
 
 
-# @user_routes.route('/user/delete/<string:user_id>', methods=["DELETE"])
+# @user_routes.route('/delete/<string:user_id>', methods=["DELETE"])
 # def delete_user(user_id):
 #     try:
 #         user_id = ObjectId(user_id)
@@ -62,7 +63,7 @@ def create_users():
 #             return jsonify({"error": "User not found"}), 404
 #     return jsonify ({'message': f'deleting user with ID {user_id}'})
 
-# @user_routes.route("/user/update/<string:user_id>", methods=["PUT"])
+# @user_routes.route("/update/<string:user_id>", methods=["PUT"])
 # def update_user(user_id):
 #     mongo = current_app.config['MONGO']
 #     user_model = UserModel(mongo)
