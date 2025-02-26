c6bd631e26435c56e85c1c0b251aadf41f40ba96
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-02-26 17:32:20 -0500

    Update auth_routes.py

--- back_end/app/routes/auth_routes.py
+++ back_end/app/routes/auth_routes.py
@@ -3,32 +3,30 @@
 from flask import Blueprint, jsonify, current_app, request
 from flask_bcrypt import Bcrypt
 from app.models.user import UserModel
+
 # used to convert string to ObjectId
 from bson import ObjectId
 
 auth_routes = Blueprint("auth_routes", __name__)
 
 
-
 # Define a simple route inside this blueprint
-@auth_routes.route("/log_in/<string:user_id>" , methods=["POST"])
+@auth_routes.route("/log_in/<string:user_id>", methods=["POST"])
 def log_in(user_id):
-    
     data = request.get_json()
     username = data["username"]
     password = data["password"]
     bcrypt = Bcrypt(current_app)
-    #retrive hasshed password from the database
+    # retrive hasshed password from the database
     user = UserModel(current_app.mongo)
     user_id = ObjectId(user_id)
     response = user.find_user_by_id(user_id)
-    print(response)
+    print(response["password"])
     hashed_password = data["hashed_password"]
-    is_valid = bcrypt.check_password_hash(hashed_password, 'password')
-    return jsonify({'username': 'here', 'password': 'List'})
+    is_valid = bcrypt.check_password_hash(hashed_password, "password")
+    return jsonify({"username": "here", "password": "List"})
 
-  
 
 @auth_routes.route("/log_out")
 def log_out():
-  return jsonify({'message': 'List will be here'})
+    return jsonify({"message": "List will be here"})
