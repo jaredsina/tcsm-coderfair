785961f5c3319bf2a59c24c603a3168999d2d8e4
SiddharthP711 <sid711p@gmail.com>
2025-02-28 16:19:16 -0500

    Single Project Page
    Created the Single Project Page

--- client/src/Pages/Single-ProjectPage/SingleProject.jsx
+++ client/src/Pages/Single-ProjectPage/SingleProject.jsx
@@ -1,30 +1,126 @@
-import { Flex, Text, List, ListItem, ActionIcon } from '@mantine/core';
+import {
+  Flex,
+  Text,
+  List,
+  ListItem,
+  Image,
+  Button,
+  Anchor,
+} from '@mantine/core';
+import { CiShare1 } from 'react-icons/ci';
+import { BiLike } from 'react-icons/bi';
+import { FaRegShareSquare } from 'react-icons/fa';
+
+import { useState } from 'react';
 
 export default function SingleProject() {
+  const [liked, setLiked] = useState(false); // tells us if the project is liked
+  const [shared, setShared] = useState(false);
+
   return (
-    <Flex pl={875}>
-      <Flex direction={'column'} bg="rgba(0, 148, 7, .8)" w={540} h={527}>
-        <Text size="xl" fw={700} Bold tt="uppercase" pl={185}>
+    <Flex gap={90} direction={{base:'column', md:'row', sm:"column"}} align={'center'}>
+      <Flex direction={'column'} pl={'100'} gap={'25'} flex={1}>
+        <Flex direction={'row'} gap={40} pt={50}>
+          <Text size="1.5rem" fw={700} Bold>
+            Score/Rank
+          </Text>
+          <Flex direction={'column'} ta={'center'}>
+            <Anchor href={'https://flappybird.io/'} target="_blank">
+              <Button
+                variant="light"
+                color="green"
+                leftSection={<CiShare1 size={40} />}
+              >
+                <Text fw={700} Bold ta={'center'}>
+                  Open in New Tab
+                </Text>
+              </Button>
+            </Anchor>
+          </Flex>
+          <Flex direction={'row'} gap={40}>
+            <Button
+              variant="light"
+              color={liked === true ? 'green' : 'red'}
+              leftSection={<BiLike size={40} />}
+              onClick={function () {
+                console.log('pressed');
+                setLiked(true);
+              }}
+            >
+              <Text fw={700} Bold>
+                Like
+              </Text>
+            </Button>
+            <Button
+              variant="light"
+              color={shared === true ? 'green' : 'red'}
+              leftSection={<FaRegShareSquare size={40} />}
+              onClick={function () {
+                setShared(true);
+                navigator.clipboard.writeText('https://flappybird.io/');
+              }}
+            >
+              <Text fw={700} Bold ta={'center'}>
+                {shared === true ? 'Copied' : 'Copy'}
+              </Text>
+            </Button>
+          </Flex>
+        </Flex>
+        <Image
+          radius="md"
+          src={
+            'https://lincolnliontales.com/wp-content/uploads/2014/02/Flappy-Bird-Teaser.jpg'
+          }
+          h={380}
+          w="auto"
+        ></Image>
+      </Flex>
+      <Flex
+        direction={'column'}
+        bg="rgba(0, 145, 7, .8)"
+        w={455}
+        h= {'100%'}
+        gap={40}
+      >
+        <Text size="2rem" fw={700} Bold tt="uppercase" pl={200} pt={30}>
           Info:
         </Text>
-        <List withPadding>
-          <List.Item ta={'center'} fw={1000}>
-            Name of Project:
-            <ListItem> Flappy Bird</ListItem>
-          </List.Item>
-          <List.Item ta={'center'} size="xl" fw={1000}>
-            Name of Coder:
-            <ListItem>Sid</ListItem>
-          </List.Item>
-          <List.Item ta={'center'} size="xl" fw={1000}>
-            Coding Language:
-            <ListItem>Python</ListItem>
-          </List.Item>
-          <List.Item ta={'center'} size="xl" fw={1000}>
-            Coder Skill Level
-            <ListItem>Pro</ListItem>
-          </List.Item>
-        </List>
+        <Flex direction={'column'}>
+          <List withPadding spacing={30}>
+            <List.Item ta={'center'}>
+              <Text size="1.5rem" fw={600} Bold>
+                Name of Project:
+              </Text>
+              <ListItem>
+                <Text size="1.25rem">Flappy Bird</Text>
+              </ListItem>
+            </List.Item>
+            <List.Item ta={'center'}>
+              <Text size="1.5rem" fw={600} Bold>
+                Name of Coder:
+              </Text>
+              <ListItem>
+                <Text size="1.25rem">Sid</Text>
+              </ListItem>
+            </List.Item>
+            <List.Item ta={'center'} size="xl">
+              <Text size="1.5rem" fw={600} Bold>
+                Coding Language:
+              </Text>
+              <ListItem>
+                <Text size="1.25rem">Python</Text>
+              </ListItem>
+            </List.Item>
+            <List.Item ta={'center'} size="xl">
+              <Text size="1.5rem" fw={600} Bold>
+                Coder Skill Level
+              </Text>
+              <ListItem>
+                <Text size="1.25rem">Pro</Text>
+              </ListItem>
+            </List.Item>
+          </List>
+        </Flex>
       </Flex>
     </Flex>
   );
