76c40c6e8c23f9f01f254647980fa4b8140d126d
Ishita-mathur1 <blossome.jake@gmail.com>
2025-03-01 10:52:45 -0500

    update project method

--- back_end/app/routes/projects_routes.py
+++ back_end/app/routes/projects_routes.py
@@ -53,37 +53,37 @@ def delete_project(project_id):
     # If no exceptions are raised, return a success message and status code 200
     return jsonify({"message": f"project with ID {project_id}"}), 200
 
-# @projects_routes.route('/update/<string:project_id>', methods = ["PUT"])
-# def update_project(project_id):
-#     try:
-#         # we will get the project_id from the request json
-#         data = request.get_json()
-#         print(data)
-#         # updated_project = data["updated_judge"]
-#         update_project = {
-#             "student_id": ObjectId(data["student_id"]),
-#             "coderfair_id": ObjectId(data["coderfair_id"]),
-#             "name": data["name"],
-#             "description": data["description"],
-#             "category": data["category"],
-#             "project_image": data["project_image"],
-#             "presentation_video_url": data["presentation_video_url"],
-#             "code_access_link": data["code_access_link"],
-#             "coding_language": data["coding_language"],
-#             "project_username": data["project_username"],
-#             "project_password": data["project_password"],
-#             "notes": data["notes"],
-#         }
-#         new_project = ProjectModel(current_app.mongo)
-#         response = new_project.update_project(project_id, update_project)
+@projects_routes.route('/update/<string:project_id>', methods = ["PUT"])
+def update_project(project_id):
+    try:
+        # we will get the project_id from the request json
+        data = request.get_json()
+        print(data)
+        # updated_project = data["updated_judge"]
+        update_project = {
+            "student_id": ObjectId(data["student_id"]),
+            "coderfair_id": ObjectId(data["coderfair_id"]),
+            "name": data["name"],
+            "description": data["description"],
+            "category": data["category"],
+            "project_image": data["project_image"],
+            "presentation_video_url": data["presentation_video_url"],
+            "code_access_link": data["code_access_link"],
+            "coding_language": data["coding_language"],
+            "project_username": data["project_username"],
+            "project_password": data["project_password"],
+            "notes": data["notes"],
+        }
+        new_project = ProjectModel(current_app.mongo)
+        response = new_project.update_project(ObjectId(project_id), update_project)
 
-#     except Exception as e:
-#         print(e)
-#         # If an exception is raised, return an error message and status code 400
-#         return jsonify({"message":"Error updating project", "error":str(e)}), 400
+    except Exception as e:
+        print(e)
+        # If an exception is raised, return an error message and status code 400
+        return jsonify({"message":"Error updating project", "error":str(e)}), 400
   
-#     # If no exceptions are raised, return a success message and status code 200
-#     return jsonify(["message": str(response)]), 200
+    # If no exceptions are raised, return a success message and status code 200
+    return jsonify({"message": str(response)}), 200
 
 # @projects_routes.route('/create/<string:project_id>', methods = ["POST"])
 # def create_project(project_id):
