607efb7d3170b13366ac4d3ab39c044e4491069c
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-03-06 14:41:00 -0500

    Use database data on project page

--- client/src/Pages/ProjectPage/ProjectPage.jsx
+++ client/src/Pages/ProjectPage/ProjectPage.jsx
@@ -2,7 +2,19 @@ import { Flex, Button, Title } from '@mantine/core';
 import ProjectData from '../../data/projects.json';
 import ProjectCard from '../../Components/ProjectCard/ProjectCard';
 import SearchBar from '../../Components/SearchBar/SearchBar';
+import { fetchProjects } from '../../reducers/projectSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 const ProjectPage = () => {
+
+  const projects = useSelector((state)=>state.projects.projects)
+  const projectStatus = useSelector((state)=>state.projects.status)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    projectStatus === "idle" ? dispatch(fetchProjects()) : null;
+  }, [projectStatus, dispatch])
 
   return (
     <main className="ProjectPageMain">
@@ -13,13 +25,9 @@ const ProjectPage = () => {
       <SearchBar />
 
       <Flex mih={50} gap="md" direction="row" justify="center" wrap="wrap">
-        {ProjectData.map((project) => (
-          <div className="ProjectCard">
-            <ProjectCard
-              title={project.title}
-              description={project.description}
-              language={project.language}
-            />
+        {projects.map((project, index) => (
+          <div className="ProjectCard" key={index}>
+            <ProjectCard title={project.name} language={project.coding_language ? project.coding_language : "Other"} description={project.description} image={project.project_image ? project.project_image : "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"}/>
           </div>
         ))}
       </Flex>
